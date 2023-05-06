d3970f4b9343b770790cb47acc5753e139df20b1
Your Name <urielparavi75@gmail.com>
2023-05-06 20:10:08 +0300

    22

--- javaScript.js
+++ javaScript.js
@@ -1,10 +1,16 @@
-// Write a function which takes in a string and returns counts of each character in the string.
+// chartCount("Your PIN number is 1234");  => example to the tesk
 
-charCount('aaaa'); // {a:4}
-charCount('hello'); // {h:1, e:1, l:2, o:1}
+function chartCount(str) {
+  // do something
+  // return an object with keys that are lowercase alphanumeric characters in the string;
+  // values should be  the counts for those characters
+}
 
-// Example to more complex inputs
-// "my phone number is 182763"
-// "Hello hi"
-
-charCount('');
+function chartCount(str) {
+  // Make object to return at end
+  // loop over string, for each character...
+  // if the char is a number/letter AND is a key in object, add one to count
+  // if the char is a number/letter AND not in object, add it to object and set value to 1
+  //if character is something else (space, period, etc..) don't do anything
+  // return object at end
+}
