d3515d645cca2eb5115987a87f47cdff192fe564
Your Name <urielparavi75@gmail.com>
2023-05-06 19:16:42 +0300

    21

---
+++ javaScript.js
@@ -0,0 +1,10 @@
+// Write a function which takes in a string and returns counts of each character in the string.
+
+charCount('aaaa'); // {a:4}
+charCount('hello'); // {h:1, e:1, l:2, o:1}
+
+// Example to more complex inputs
+// "my phone number is 182763"
+// "Hello hi"
+
+charCount('');
