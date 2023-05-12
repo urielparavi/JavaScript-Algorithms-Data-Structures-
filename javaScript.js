891914686b3a7d92e5ace1f1bfba25b1fba3652a
Your Name <urielparavi75@gmail.com>
2023-05-12 21:35:28 +0300

    34

--- javaScript.js
+++ javaScript.js
@@ -1,14 +1,37 @@
-const countUniqueValues = (arr) => {
-  if (arr.length === 0) return 0;
-  var i = 0;
-  for (var j = 1; j < arr.length; j++) {
-    if (arr[i] !== arr[j]) {
-      i++;
-      arr[i] = arr[j];
-    }
-    // console.log(i, j);
+// const maxSubarraySum = (arr, num) => {
+//   if (num > arr.length) {
+//     return null;
+//   }
+//   var max = -Infinity;
+//   for (let i = 0; i < arr.length - num + 1; i++) {
+//     temp = 0;
+//     for (let j = 0; j < num; j++) {
+//       temp += arr[i + j];
+//     }
+//     if (temp > max) {
+//       max = temp;
+//     }
+//     console.log(temp, max);
+//   }
+//   return max;
+// };
+
+// // console.log(maxSubarraySum([2, 6, 9, 2, 1, 8, 5, 6, 3], 3));
+// maxSubarraySum([2, 6, 9, 2, 1, 8, 5, 6, 3], 3);
+
+const maxSubarraySum = (arr, num) => {
+  let maxSum = 0;
+  let tempSum = 0;
+  if (arr.length < num) return null;
+  for (let i = 0; i < num; i++) {
+    maxSum += arr[i];
   }
-  return i + 1;
+  tempSum = maxSum;
+  for (let i = num; i < arr.length; i++) {
+    tempSum = tempSum - arr[i - num] + arr[i];
+    maxSum = Math.max(maxSum, tempSum);
+  }
+  return maxSum;
 };
 
-console.log(countUniqueValues([1, 1, 1, 2, 2, 3, 4, 5, 5]));
+console.log(maxSubarraySum([2, 6, 9, 2, 1, 8, 5, 6, 3], 3));
