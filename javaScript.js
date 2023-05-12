10627e26cee3b3ec8eb31b70db66fd240063ee0f
Your Name <urielparavi75@gmail.com>
2023-05-12 19:42:52 +0300

    32

--- javaScript.js
+++ javaScript.js
@@ -1,16 +1,14 @@
-const sumZero = (arr) => {
-  let left = 0;
-  let right = arr.length - 1;
-  while (left < right) {
-    let sum = arr[left] + arr[right];
-    if (sum === 0) {
-      return [arr[left], arr[right]];
-    } else if (sum > 0) {
-      right--;
-    } else {
-      left++;
+const countUniqueValues = (arr) => {
+  if (arr.length === 0) return 0;
+  var i = 0;
+  for (var j = 1; j < arr.length; j++) {
+    if (arr[i] !== arr[j]) {
+      i++;
+      arr[i] = arr[j];
     }
+    // console.log(i, j);
   }
+  return i + 1;
 };
 
-console.log(sumZero([-4, -5, -6, -8, 0, 1, 2, 3, 10]));
+console.log(countUniqueValues([1, 1, 1, 2, 2, 3, 4, 5, 5]));
