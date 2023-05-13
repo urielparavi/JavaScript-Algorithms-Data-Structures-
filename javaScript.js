2fa3477cefc1798930ef0f3a8c0f3d1cd0f32751
Your Name <urielparavi75@gmail.com>
2023-05-13 18:55:17 +0300

    49

--- javaScript.js
+++ javaScript.js
@@ -1,7 +1,22 @@
-// 47. Writing Factorial Recursively
-const factorial = (num) => {
-  if (num === 1) return 1;
-  return num * factorial(num - 1);
+// 49. Helper Method Recursion
+const collectOddValues = (arr) => {
+  let result = [];
+
+  const helper = (helperInput) => {
+    if (helperInput.length === 0) {
+      return;
+    }
+
+    if (helperInput[0] % 2 !== 0) {
+      result.push(helperInput[0]);
+    }
+    // We call it on everything else in the array, excluding that first element
+    helper(helperInput.slice(1));
+  };
+
+  helper(arr);
+
+  return result;
 };
 
-console.log(factorial(5));
+console.log(collectOddValues([1, 2, 3, 4, 5, 6, 7, 8, 9]));
