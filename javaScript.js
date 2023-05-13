2dec5ac58a116fc2c836c352252dd57f981ac481
Your Name <urielparavi75@gmail.com>
2023-05-13 18:02:51 +0300

    46

--- javaScript.js
+++ javaScript.js
@@ -1,13 +1,10 @@
-// Recursion with Call stack and Break point
-const sumRange = (num) => {
-  if (num === 1) return 1;
-  return num + sumRange(num - 1);
+// 46. Writing Factorial Iteratively
+const factorial = (num) => {
+  let total = 1;
+  for (let i = num; i > 1; i--) {
+    total *= i;
+  }
+  return total;
 };
 
-console.log(sumRange(4));
-
-// sumRange(3)
-//   return 3 + sumRange(2)
-//                return 2 + sumRange(1)
-//                             return 1
-// 3 + 2 + 1 = 6
+console.log(factorial(4));
