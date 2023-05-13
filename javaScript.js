0fbd53e35b2b6e2176f090f0e71c41766e14ddf4
Your Name <urielparavi75@gmail.com>
2023-05-13 14:20:46 +0300

    43

--- javaScript.js
+++ javaScript.js
@@ -1,31 +1,21 @@
-// Linear Search
-
-// const search = (arr, val) => {
-//   for (let i = 0; i < arr.length; i++) {
-//     if (arr[i] === val) {
-//       return i;
-//     }
-//   }
-//   return -1;
-// };
-
-// Refactor
-
-const search = (array, val) => {
-  let min = 0;
-  let max = array.length - 1;
-
-  while (min <= max) {
-    let middle = Math.floor((min + max) / 2);
-    let currentElement = array[middle];
-
-    if (array[midlle] < val) {
-      min = middle + 1;
-    } else if (array[middle] > val) {
-      max = middle - 1;
-    } else {
-      return middle;
-    }
-  }
-  return -1;
+const takeShower = () => {
+  return 'Showering!';
 };
+
+const eatBreakfast = () => {
+  let meal = cookFood();
+  return `Eating ${meal}`;
+};
+
+const cookFood = () => {
+  let items = ['Oatmeal', 'Eggs', 'Protein Shake'];
+  return items[Math.floor(Math.random() * items.length)];
+};
+
+const wakeUp = () => {
+  takeShower();
+  eatBreakfast();
+  console.log('Ok ready to go to work!');
+};
+
+wakeUp();
