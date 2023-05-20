9f2f3a3d90fcca53d38349304fb18c25ea192fc8
Your Name <urielparavi75@gmail.com>
2023-05-20 19:03:47 +0300

    72-73

--- javaScript.js
+++ javaScript.js
@@ -1,22 +1,22 @@
-// 70. Bubble Sort: Overview
+// 72. Bubble Sort: Optimization
 
+// Optimized with noSwaps
 const bubbleSort = (arr) => {
+  var noSwaps;
   for (var i = arr.length; i > 0; i--) {
+    noSwaps = true;
     for (var j = 0; j < i - 1; j++) {
       console.log(arr, arr[j], arr[j + 1]);
       if (arr[j] > arr[j + 1]) {
-        // SWAP!
         var temp = arr[j];
         arr[j] = arr[j + 1];
         arr[j + 1] = temp;
+        noSwaps = false;
       }
     }
-    // console.log('ONE PASS COMPLETE!');
+    if (noSwaps) break;
   }
   return arr;
 };
 
-console.log(bubbleSort([37, 45, 29, 8, 12, 88, -3]));
-// [37, 45, 29, 8]
-// [37, 29, 8, 45]
-// [29, 8, 37, 45]
+console.log(bubbleSort([8, 1, 2, 3, 4, 5, 6, 7]));
