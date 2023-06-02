22ffe4208ac7f1398cb9ae1c537a34e0e1a57bcc
Your Name <urielparavi75@gmail.com>
2023-06-02 17:39:27 +0300

    80

--- javaScript.js
+++ javaScript.js
@@ -1,25 +1,21 @@
-// 76. Selection Sort: Implementation
+// 80. Insertion Sort: Implementation
 
-const selectionSort = (arr) => {
-  for (var i = 0; i < arr.length; i++) {
-    var lowest = i;
-    for (var j = i + 1; j < arr.length; j++) {
-      if (arr[j] < arr[lowest]) {
-        lowest = j;
-      }
+const insertionSort = (arr) => {
+  for (var i = 1; i < arr.length; i++) {
+    var currentVal = arr[i];
+    for (var j = i - 1; j >= 0 && arr[j] > currentVal; j--) {
+      arr[j + 1] = arr[j];
+      console.log(arr);
     }
-    if (i !== lowest) {
-      console.log(i, lowest);
-      var temp = arr[i];
-      arr[i] = arr[lowest];
-      arr[lowest] = temp;
-    }
+    arr[j + 1] = currentVal;
   }
   return arr;
 };
 
-console.log(selectionSort([0, 2, 34, 22, 10, 19, 17]));
-// i, j, lowest
-// 0, 1, 1
-// 0, 2, 2
-// 0, 3, 2
+console.log(insertionSort([2, 1, 9, 7, 6, 4]));
+// console.log(insertionSort([4, 3, 2, 1]));
+
+// [1, 2, 9, 76, 0]
+// [0, 1, 2, 9, 76]        0
+
+// [1, 2, 9, 76]  20
