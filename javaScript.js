342925fa108b1dbb5ca259f73d706cb61b1c6191
Your Name <urielparavi75@gmail.com>
2023-06-03 19:16:18 +0300

    84-90

--- javaScript.js
+++ javaScript.js
@@ -27,4 +27,28 @@ const merge = (arr1, arr2) => {
 
 // console.log(merge([1, 10, 50], [2, 14, 99, 100]));
 // console.log(merge([], [1, 3]));
-console.log(merge([100], [1, 2, 3, 5, 6]));
+// console.log(merge([100], [1, 2, 3, 5, 6]));
+
+// 89. Writing Merge Sort Part 2
+
+const mergeSort = (arr) => {
+  if (arr.length <= 1) return arr;
+  let mid = Math.floor(arr.length / 2);
+  let left = mergeSort(arr.slice(0, mid));
+  let right = mergeSort(arr.slice(mid));
+  return merge(left, right);
+  // mergeSort();
+};
+
+console.log(mergeSort([10, 24, 76, 73]));
+
+// Example how we did get the solution
+// var arr = [10, 24, 76, 73, 72, 1, 9];
+
+// Math.floor(arr.length / 2);
+
+// The first halve
+// arr.slice(0, 3);
+
+// The second halve
+// arr.slice(3);
