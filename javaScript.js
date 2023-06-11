d3b5916e55f3390838935d832a78f5e1f3dc195f
Your Name <urielparavi75@gmail.com>
2023-06-11 19:16:03 +0300

    94-96

--- javaScript.js
+++ javaScript.js
@@ -1,54 +1,30 @@
-// 87. Merging Arrays: Implementation
+// 94. Pivot Helper Implementation
 
-const merge = (arr1, arr2) => {
-  let results = [];
-  let i = 0;
-  let j = 0;
-  while (i < arr1.length && j < arr2.length) {
-    if (arr2[j] > arr1[i]) {
-      results.push(arr1[i]);
-      i++;
-    } else {
-      results.push(arr2[j]);
-      j++;
+const pivot = (arr, start = 0, end = arr.length + 1) => {
+  const swap = (array, i, j) => {
+    let temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  };
+
+  let pivot = arr[start];
+  let swapIdx = start;
+
+  for (var i = start + 1; i < arr.length; i++) {
+    if (pivot > arr[i]) {
+      swapIdx++;
+      swap(arr, swapIdx, i);
     }
   }
-  while (i < arr1.length) {
-    results.push(arr1[i]);
-    i++;
-  }
-  while (j < arr2.length) {
-    results.push(arr2[j]);
-    j++;
-  }
-
-  return results;
+  swap(arr, start, swapIdx);
+  return swapIdx;
 };
 
-// console.log(merge([1, 10, 50], [2, 14, 99, 100]));
-// console.log(merge([], [1, 3]));
-// console.log(merge([100], [1, 2, 3, 5, 6]));
+console.log(pivot([4, 8, 2, 1, 5, 7, 6, 3]));
+// [4, 8, 2, 1, 5, 7, 6, 3]
+// [4, 2, 8, 1, 5, 7, 6, 3]
+// [4, 2, 8, 1, 5, 7, 6, 3]
+// [4, 2, 1, 8, 5, 7, 6, 3]
+// [4, 2, 1, 3, 5, 7, 6, 8]
 
-// 89. Writing Merge Sort Part 2
-
-const mergeSort = (arr) => {
-  if (arr.length <= 1) return arr;
-  let mid = Math.floor(arr.length / 2);
-  let left = mergeSort(arr.slice(0, mid));
-  let right = mergeSort(arr.slice(mid));
-  return merge(left, right);
-  // mergeSort();
-};
-
-console.log(mergeSort([10, 24, 76, 73]));
-
-// Example how we did get the solution
-// var arr = [10, 24, 76, 73, 72, 1, 9];
-
-// Math.floor(arr.length / 2);
-
-// The first halve
-// arr.slice(0, 3);
-
-// The second halve
-// arr.slice(3);
+// [3, 2, 1, 4, 5, 7, 6, 8]
