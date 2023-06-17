33f318714bf414de0c280682e35c20ee0b371585
Your Name <urielparavi75@gmail.com>
2023-06-17 14:01:47 +0300

    96-101

--- javaScript.js
+++ javaScript.js
@@ -28,3 +28,16 @@ console.log(pivot([4, 8, 2, 1, 5, 7, 6, 3]));
 // [4, 2, 1, 3, 5, 7, 6, 8]
 
 // [3, 2, 1, 4, 5, 7, 6, 8]
+
+const quickSort = (arr, left = 0, right = arr.length - 1) => {
+  if (left < right) {
+    let pivotIndex = pivot(arr, left, right);
+    // left
+    quickSort(arr, left, pivotIndex - 1);
+    // right
+    quickSort(arr, pivotIndex + 1, right);
+  }
+  return arr;
+};
+
+quickSort([4, 6, 9, 1, 2, 5]);
