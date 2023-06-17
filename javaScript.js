694bce3ef9abd8d316cdf4161ca3bfd67937e936
Your Name <urielparavi75@gmail.com>
2023-06-17 17:44:11 +0300

    102-106

--- javaScript.js
+++ javaScript.js
@@ -1,43 +1,35 @@
-// 94. Pivot Helper Implementation
+// Play 102. Radix Sort: Implementation
 
-const pivot = (arr, start = 0, end = arr.length + 1) => {
-  const swap = (array, i, j) => {
-    let temp = array[i];
-    array[i] = array[j];
-    array[j] = temp;
-  };
+const getDigit = (num, i) => {
+  return Math.floor(Math.abs(num) / Math.pow(10, i)) % 10;
+};
 
-  let pivot = arr[start];
-  let swapIdx = start;
+const digitCount = (num) => {
+  if (num === 0) {
+    return Math.floor(Math.log10(Math.abs(num))) + 1;
+  }
+};
 
-  for (var i = start + 1; i < arr.length; i++) {
-    if (pivot > arr[i]) {
-      swapIdx++;
-      swap(arr, swapIdx, i);
+const mostDigits = (nums) => {
+  let maxDigits = 0;
+  for (let i = 0; i < nums.length; i++) {
+    maxDigits = Math.max(maxDigits, digitCount(nums[i]));
+  }
+};
+
+const radixSort = (nums) => {
+  let maxDigitCount = mostDigits(nums);
+  for (let k = 0; k < maxDigitCount; k++) {
+    let digitBuckets = Array.from({ length: 10 }, () => []);
+    for (let i = 0; i < nums.length; i++) {
+      let digit = getDigit(nums[i], k);
+      digitBuckets[digit].push(nums[i]);
     }
+    console.log(digitBuckets);
+    nums = [].concat(...digitBuckets);
+    console.log(nums);
   }
-  swap(arr, start, swapIdx);
-  return swapIdx;
+  return nums;
 };
 
-console.log(pivot([4, 8, 2, 1, 5, 7, 6, 3]));
-// [4, 8, 2, 1, 5, 7, 6, 3]
-// [4, 2, 8, 1, 5, 7, 6, 3]
-// [4, 2, 8, 1, 5, 7, 6, 3]
-// [4, 2, 1, 8, 5, 7, 6, 3]
-// [4, 2, 1, 3, 5, 7, 6, 8]
-
-// [3, 2, 1, 4, 5, 7, 6, 8]
-
-const quickSort = (arr, left = 0, right = arr.length - 1) => {
-  if (left < right) {
-    let pivotIndex = pivot(arr, left, right);
-    // left
-    quickSort(arr, left, pivotIndex - 1);
-    // right
-    quickSort(arr, pivotIndex + 1, right);
-  }
-  return arr;
-};
-
-quickSort([4, 6, 9, 1, 2, 5]);
+console.log(radixSort([23, 345, 5467, 12, 2345, 9852]));
