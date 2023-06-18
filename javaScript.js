fecc0d15b783baa5242186d0d6764fa393e54b48
Your Name <urielparavi75@gmail.com>
2023-06-18 17:52:02 +0300

    111-116

--- javaScript.js
+++ javaScript.js
@@ -1,60 +1,72 @@
-// 106. Data Structures: The Class Keyword
+// 111. Starter Code and Push Intro
 
-// class Student {
-//   constructor(firstName, lastName, year) {
-//     this.firstName = firstName;
-//     this.lastName = lastName;
-//     this.grade = year;
-//     this.tardies = 0;
-//     this.scores = [];
-//   }
-//   fullName() {
-//     return `Your full name is ${this.firstName} ${this.lastName}`;
-//   }
-//   markLate() {
-//     this.tardies += 1;
-//     if (this.tardies >= 3) {
-//       return 'YOU ARE EXPELLED!';
-//     }
-//     return `${this.firstName} ${this.lastName} has been late ${this.tardies} times`;
-//   }
-//   addScore(score) {
-//     this.scores.push(score);
-//     return this.scores;
-//   }
-//   calculateAverage() {
-//     let sum = this.scores.reduce(function (a, b) {
-//       return a + b;
-//     });
-//     return sum / this.scores.length;
-//   }
-//   static EnrollStudents() {
-//     return 'ENROLLING STUDENTS!';
-//   }
-// }
+// Piece of data - val
+// Reference to next node
 
-// let firstStudent = new Student('Emil', 'katz', 3);
-// let secondStudent = new Student('Avi', 'choen', 2);
-
-// // console.log(firstStudent);
-// // console.log(secondStudent);
-// console.log(firstStudent.fullName());
-
-class Point {
-  constructor(x, y) {
-    this.x = x;
-    this.y = y;
-  }
-
-  static distance(a, b) {
-    const dx = a.x - b.x;
-    const dy = (a.y = b.y);
-
-    return Math.hypot(dx, dy);
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
   }
 }
 
-const p1 = new Point(5, 5);
-const p2 = new Point(10, 10);
+class SinglyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+  // Insert at the end
+  push(val) {
+    let newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+  pop() {
+    if (!this.head) return undefined;
+    let current = this.head;
+    let newTail = current;
+    while (current.next) {
+      newTail = current;
+      current = current.next;
+    }
+    this.tail = newTail;
+    this.tail.next = null;
+    this.length--;
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+    return current;
+  }
+  // traverse() {
+  // traverse() {
+  //   let current = this.head;
+  //   while (current) {
+  //     console.log(current.val);
+  //     current = current.next;
+  //   }
+  // }
+}
 
-console.log(Point.distance(p1, p2));
+let list = new SinglyLinkedList();
+list.push('HELLO');
+list.push('GOODBYE');
+list.push('!');
+
+// HELLO -> GOODBYE -> !
+// c
+// nt
+
+// let first = new Node('Hi');
+// first.next = new Node('there');
+// first.next.next = new Node('how');
+// first.next.next.next = new Node('are');
+// first.next.next.next.next = new Node('you');
