717d3c3bcac465d87d20529911c12f5bd9e19d1f
Your Name <urielparavi75@gmail.com>
2023-07-07 20:30:02 +0300

    152

--- javaScript.js
+++ javaScript.js
@@ -1,134 +1,25 @@
-// 132. Setting Up Our Node Class
+// 152. Creating a Stack with an Array
+let stack = [];
+stack.push('google');
 
-class Node {
-  constructor(val) {
-    this.val = val;
-    this.next = null;
-    this.prev = null;
-  }
-}
+stack.push('instagram');
 
-class DoublyLinkedList {
-  constructor() {
-    this.head = null;
-    this.tail = null;
-    this.length = 0;
-  }
-  push(val) {
-    let newNode = new Node(val);
-    if (this.length === 0) {
-      this.head = newNode;
-      this.tail = newNode;
-    } else {
-      this.tail.next = newNode;
-      newNode.prev = this.tail;
-      this.tail = newNode;
-    }
-    this.length++;
-    return this;
-  }
-  pop() {
-    if (!this.head) return undefined;
-    let poppeNode = this.tail;
-    if (this.length === 1) {
-      this.head = null;
-      this.tail = null;
-    } else {
-      this.tail = poppeNode.prev;
-      this.tail.next = null;
-      poppeNode.prev = null;
-    }
-    this.length--;
-    return poppeNode;
-  }
-  shift() {
-    if (this.length === 0) return undefined;
-    let oldHead = this.head;
-    if (this.length === 1) {
-      this.head = null;
-      this.tail = null;
-    } else {
-      this.head = oldHead.next;
-      this.head.prev = null;
-      oldHead.next = null;
-    }
-    this.length--;
-    return oldHead;
-  }
-  unshift(val) {
-    let newNode = new Node(val);
-    if (this.length === 0) {
-      this.head = newNode;
-      this.tail = newNode;
-    } else {
-      this.head.prev = newNode;
-      newNode.next = this.head;
-      this.head = newNode;
-    }
-    this.length++;
-    return this;
-  }
-  get(index) {
-    if (index < 0 || index >= this.length) return null;
-    let count, current;
-    if (index <= this.length / 2) {
-      count = 0;
-      current = this.head;
-      while (count != index) {
-        current = current.next;
-        count++;
-      }
-    } else {
-      count = this.length - 1;
-      current = this.tail;
-      while (count !== index) {
-        current = current.prev;
-        count--;
-      }
-    }
-    return current;
-  }
-  set(index, val) {
-    let foundNode = this.get(index);
-    if (foundNode != null) {
-      foundNode.val = val;
-      return true;
-    }
-    return false;
-  }
-  insert(index, val) {
-    if (index < 0 || index > this.length) return false;
-    if (index === 0) return this.unshift(val);
-    if (index === this.length) return this.push(val);
+stack.push('youtube');
 
-    let newNode = new Node(val);
-    let beforeNode = this.get(index - 1);
-    let afterNode = beforeNode.next;
+stack.pop();
 
-    (beforeNode.next = newNode), (newNode.prev = beforeNode);
-    (newNode.next = afterNode), (afterNode.prev = newNode);
-    this.length++;
-    return true;
-  }
-  remove(index) {
-    if (index < 0 || index >= this.length) return undefined;
-    if (index === 0) return this.shift();
-    if (index === this.length - 1) return this.pop();
-    let removedNode = this.get(index);
-    let beforeNode = removedNode.prev;
-    let afterNode = removedNode.next;
-    beforeNode.next = afterNode;
-    afterNode.prev = beforeNode;
-    // removedNode.prev.next = removedNode.next;
-    // removedNode.next.prev = removedNode.prev;
-    removedNode.next = null;
-    removedNode.prev = null;
-    this.length--;
-    return removedNode;
-  }
-}
+stack.pop();
 
-let list = new DoublyLinkedList();
-list.push('Harry');
-list.push('Ron');
-list.push('Hermione');
+stack.push('amazon');
+
+stack.pop();
+
+stack.unshift('create new file');
+
+stack.unshift('resized file');
+
+stack.unshift('cloned out wrinkle');
+
+stack.shift();
+
+console.log(stack);
