57c9161e301316da1a7e75e88873b7971aac9431
Your Name <urielparavi75@gmail.com>
2023-08-09 10:48:33 +0300

    165-170

--- javaScript.js
+++ javaScript.js
@@ -1,37 +1,88 @@
-// 157. Writing Our Own Queue From Scratch
+// 165. Our Tree Classes
 class Node {
   constructor(value) {
     this.value = value;
-    this.next = null;
+    this.left = null;
+    this.right = null;
   }
 }
 
-class Queue {
+class BinarySearchTree {
   constructor() {
-    this.first = null;
-    this.last = null;
-    this.size = 0;
+    this.root = null;
   }
-  enqueue(val) {
-    let newNode = new Node(val);
-    if (!this.first) {
-      this.first = newNode;
-      this.last = newNode;
+  insert(value) {
+    let newNode = new Node(value);
+    if (this.root === null) {
+      this.root = newNode;
+      return this;
     } else {
-      this.last.next = newNode;
-      this.last = newNode;
-    }
-    return ++this.size;
-  }
-  dequeue() {
-    if (!this.first) return null;
-
-    let temp = this.first;
-    if (this.first === this.last) {
-      this.last = null;
-    }
-    this.first = this.first.next;
-    this.size--;
-    return temp.value;
-  }
+      let current = this.root;
+      while (true) {
+        if (value === current.value) return undefined;
+        if (value < current.value) {
+          if (current.left === null) {
+            current.left = newNode;
+            return this;
+          } else {
+            current = current.left;
+          }
+        } else if (value > current.value) {
+          if (current.right === null) {
+            current.right = newNode;
+            return this;
+          } else {
+            current = current.right;
+          }
+        }
+      }
+// find(value){
+//   if(this.root===null)return false;
+//   let current=this.root,
+//   found=false;
+//   while(current&&!found){
+// if(value<current.value){
+//   current=current.left;
+// }else if(value>current.value){
+//   current=current.right;
+// }else{
+//   found=true;
+// }
+//   }
+//   if(!found)return undefined;
+//   return current;
+// }
+//     }
+//   }
+contains(value){
+  if(this.root===null)return false;
+  let current=this.root,
+  found=false;
+  while(current&&!found){
+if(value<current.value){
+  current=current.left;
+}else if(value>current.value){
+  current=current.right;
+}else{
+ return true;
 }
+  }
+  return false;
+}
+    }
+  
+
+let tree = new BinarySearchTree();
+// tree.root = new Node(10);
+// tree.root.right = new Node(15);
+// tree.root.left = new Node(7);
+// tree.root.left.right = new Node(9);
+tree.insert(10);
+tree.insert(5);
+tree.insert(13);
+tree.insert(11);
+tree.insert(2);
+tree.insert(16);
+tree.insert(7);
+
+console.log(tree);
